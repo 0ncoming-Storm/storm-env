004fa523d99317fb9c280cf00cf24c173c921a4a
Vitez Orban-Imreh <ironwood421@gmail.com>
2026-09-09 15:11:20 -0600

    Sync package manifest

--- bootstrap.sh
+++ bootstrap.sh
@@ -105,5 +105,5 @@ if [ -f "$BIN_DIR/nvim" ]; then
     echo " [✓] Neovim configuration synced."
 fi
 
-install_github_bin "fastfetch" "https://github.com/fastfetch-cli/fastfetch/releases/download/2.68.1/fastfetch-dragonfly-amd64.zip" "fastfetch"
+install_github_bin "fastfetch" "fastfetch-cli/fastfetch" "fastfetch-dragonfly-amd64.zip"
 echo "=== Sync Complete ==="
