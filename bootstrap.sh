f4cabdcba7a83f318638b96ae9fde42eb685fdca
Vitez Orban-Imreh <ironwood421@gmail.com>
2026-09-09 15:08:05 -0600

    Add fastfetch package

--- bootstrap.sh
+++ bootstrap.sh
@@ -105,4 +105,5 @@ if [ -f "$BIN_DIR/nvim" ]; then
     echo " [✓] Neovim configuration synced."
 fi
 
+install_github_bin "fastfetch" "https://github.com/fastfetch-cli/fastfetch/releases/latest/download/fastfetch-linux-amd64.tar.gz" "fastfetch"
 echo "=== Sync Complete ==="
